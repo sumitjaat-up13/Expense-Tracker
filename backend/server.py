94ca4e34e0afdbf948282d5afff9fe7cc590d83b
sumit chaudhary <143445405+sumitjaat-up13@users.noreply.github.com>
2025-09-02 03:04:40 +0530

    server.py

--- backend/server.py
+++ backend/server.py
@@ -7,8 +7,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-app = FastAPI()
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -62,3 +60,4 @@ def get_analytics(date_range: DateRange):
 
 
     return breakdown
+
