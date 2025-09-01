56eda612ea506b8cd81ff49a7f7c80162d6601c8
sumit chaudhary <143445405+sumitjaat-up13@users.noreply.github.com>
2025-09-02 01:58:40 +0530

    app.py

--- frontend/app.py
+++ frontend/app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from add_update_ui import add_update_tab
 from analytics_ui import analytics_tab
 
-API_URL = "https://web-production-560bc-c06-47fd-b7bb-6ba79d1d.up.railway.app"
+API_URL = "https://web-production-523d5.up.railway.app"
 
 st.title("Expense Tracking System")
 
@@ -12,5 +12,6 @@ with tab1:
     add_update_tab()
 with tab2:
     analytics_tab()
+
 
 
