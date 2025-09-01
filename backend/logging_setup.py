90008c72357d9946770dbec1b5fa5ad4b8a9f313
sumit chaudhary <143445405+sumitjaat-up13@users.noreply.github.com>
2025-09-02 00:25:24 +0530

    Add files via upload

---
+++ backend/logging_setup.py
@@ -0,0 +1,17 @@
+import logging
+
+def setup_logger(name,log_file = 'server.log',level=logging.DEBUG):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger(name)
+
+    logger.setLevel(logging.DEBUG)
+    file_handler = logging.FileHandler(log_file)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    return logger
+
+
