77c3981076d8e415df8349e501ceb1b924736c8d
sumit chaudhary <143445405+sumitjaat-up13@users.noreply.github.com>
2025-09-02 23:04:15 +0530

    analytics_ui.py

--- frontend/analytics_ui.py
+++ frontend/analytics_ui.py
@@ -8,7 +8,7 @@ API_URL = "https://web-production-523d5.up.railway.app"
 def analytics_tab():
     col1, col2 = st.columns(2)
     with col1:
-        start_date = st.date_input("Start Date", datetime(2024, 8, 1) )
+        start_date = st.date_input("Start Date", datetime(2024, 8, 1))
 
     with col2:
         end_date = st.date_input("End Date", datetime(2024, 8, 5))
@@ -19,23 +19,33 @@ def analytics_tab():
             "end_date": end_date.strftime("%Y-%m-%d")
         }
 
-        response =  requests.post(f"{API_URL}/analytics", json=payload)
-        response = response.json()
+        response = requests.post(f"{API_URL}/analytics", json=payload)
 
-        data = {
+        try:
+            data = response.json()
+        except ValueError:
+            st.error("API ne invalid response diya:")
+            st.text(response.text)
+            return
+
+        # Response JSON ko variable me assign karo
+        response = data  
+
+        data_dict = {
             "Category": list(response.keys()),
             "Total": [response[category]["total"] for category in response],
             "Percentage": [response[category]["percentage"] for category in response]
         }
 
-        df = pd.DataFrame(data)
+        df = pd.DataFrame(data_dict)
         df_sorted = df.sort_values(by="Percentage", ascending=False)
 
-
         st.title("Expense Breakdown By Category")
-        st.bar_chart(data = df_sorted.set_index("Category")["Percentage"],width=0,height=0, use_container_width=True)
+        st.bar_chart(
+            data=df_sorted.set_index("Category")["Percentage"],
+            use_container_width=True
+        )
 
         df_sorted["Total"] = df_sorted["Total"].map("{:,.2f}".format)
         df_sorted["Percentage"] = df_sorted["Percentage"].map("{:,.2f}".format)
         st.table(df_sorted)
-
